c5e655a559e1ac9fae2d0848415342d3d3237200
WaveDev-UK <wavedevelopermc@gmail.com>
2022-12-27 00:58:55 +0000

    Added command register

--- src/main/java/org/stellardev/wavecore/plugin/WavePlugin.java
+++ src/main/java/org/stellardev/wavecore/plugin/WavePlugin.java
@@ -1,9 +1,13 @@
 package org.stellardev.wavecore.plugin;
 
+import lombok.SneakyThrows;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.stellardev.wavecore.command.WaveCommand;
+
+import java.lang.reflect.Field;
 
 public abstract class WavePlugin extends JavaPlugin {
 
@@ -13,9 +17,14 @@ public abstract class WavePlugin extends JavaPlugin {
         }
     }
 
+    @SneakyThrows
     public void registerCommands(WaveCommand... commands){
-        for(WaveCommand command : commands) {
+        final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+        bukkitCommandMap.setAccessible(true);
+        CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
 
+        for(WaveCommand command : commands) {
+            commandMap.register(command.getLabel(), command);
         }
     }
 
